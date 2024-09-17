471e99dbf1c2dbe13cb39a6ec4f28e13ea0ab79c
DennisDavydov <denisdavydov110893@gmail.com>
2024-09-17 21:51:15 +0300

    added a search menu for yle

--- uniwebscraper.py
+++ uniwebscraper.py
@@ -1,12 +1,66 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
-from argparse import ArgumentParser
 
 process = CrawlerProcess(get_project_settings())
 
-print("Type keyword to look for:")
+#All currently supported domains
+allowed_domains = ['vauva.fi', 'yle.fi']
 
-key = input()
 
-process.crawl("vauva", start_urls = [f'https://www.vauva.fi/haku?keys={key}&sort&searchpage'])
-process.start() 
+def get_input(allowed):
+    while True:
+        searchconstruct = input()
+        if searchconstruct in allowed:
+            return allowed[searchconstruct]
+        else:
+             print("Not applicable input, try again:")
+
+print("Choose domain:")
+print("\n".join(allowed_domains))
+while True:
+    domain = input()
+    if domain in allowed_domains:
+        break
+    else:
+        print('Wrong domain, try again:')
+print("------")
+#Vauva scrape code using the search function
+if domain == 'vauva.fi':
+        print("Input search term to find articles:\n")
+        key = input()
+        process.crawl("vauva", start_urls = [f'https://www.vauva.fi/haku?keys={key}&sort&searchpage'])
+        process.start() 
+
+#Yle scrape code using the search function        
+elif domain == 'yle.fi':
+    url = []
+    
+    fields = {'uutiset' : 'service=uutiset', 'urheilu' : 'service=urheilu', 'oppiminen' : 'service=oppiminen',
+               'elävä-arkisto': 'service=elava-arkisto', 'ylex' : 'service=ylex', 'kaikki' : ''}
+    print('Search in: \n' + "\n".join(fields))
+    url.append(get_input(fields))
+
+
+
+
+    times = {'tänään' : 'time=today', 'viikko': 'time=week', 'kuukausi' : 'time=month', 'anytime': '' }         
+    print('Choose time period:\n' + "\n".join(times))
+    url.append(get_input(times))
+    
+    
+    
+    lang = {'suomi': '', 'svenska': 'language=sv', 'english': 'language=en', 'russian': 'language=ru',
+             'samegiella': 'language=se', 'karjala': 'language=krl', 'kaikki': 'language=all'}
+    print('Choose language of the articles:\n' + '\n'.join(lang))
+    url.append(get_input(lang))
+    
+    print('Input search query:')
+    queryurl="query="
+    query=input()
+    url.append(queryurl + query.replace(" ", "%20"))
+    url = "&".join(url)
+    print()
+    test = f'https://haku.yle.fi/?{url}&type=article&page=1'
+    print(test)
+
+
