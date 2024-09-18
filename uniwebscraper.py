e9a905b14084a6d1c026eb3a124ae03e2b6fc611
DennisDavydov <denisdavydov110893@gmail.com>
2024-09-18 16:59:22 +0300

    prettier code

--- uniwebscraper.py
+++ uniwebscraper.py
@@ -1,5 +1,6 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
+import uh_scrapers.yle_search
 
 process = CrawlerProcess(get_project_settings())
 
@@ -11,12 +12,12 @@ def get_input(allowed_list):
     while True:
         searchconstruct = input()
         if searchconstruct in allowed_list:
-            return allowed_list[searchconstruct]
+            return allowed_list[searchconstruct], searchconstruct
         else:
              print("Not applicable input, try again:")
 
 print("Choose domain:")
-print("\n".join(allowed_domains))
+print("\n".join(allowed_domains) + "\n")
 while True:
     domain = input()
     if domain in allowed_domains:
@@ -24,43 +25,16 @@ while True:
     else:
         print('Wrong domain, try again:')
 print("------")
-#Vauva scrape code using the search function
+#Vauva scrape code using their search function
 if domain == 'vauva.fi':
         print("Input search term to find articles:\n")
         key = input()
         process.crawl("vauva", start_urls = [f'https://www.vauva.fi/haku?keys={key}&sort&searchpage'])
         process.start() 
 
-#Yle scrape code using the search function        
+#Yle scrape code using their search function      
 elif domain == 'yle.fi':
-    url = []
-    
-    fields = {'uutiset' : 'service=uutiset', 'urheilu' : 'service=urheilu', 'oppiminen' : 'service=oppiminen',
-               'elävä-arkisto': 'service=elava-arkisto', 'ylex' : 'service=ylex', 'kaikki' : ''}
-    print('Search in: \n' + "\n".join(fields) + "\n")
-    url.append(get_input(fields))
+    uh_scrapers.yle_search.yle_search_by_query()
 
-
-
-
-    times = {'tänään' : 'time=today', 'viikko': 'time=week', 'kuukausi' : 'time=month', 'anytime': '' }         
-    print('Choose time period:\n' + "\n".join(times) + "\n")
-    url.append(get_input(times))
-    
-    
-    
-    lang = {'suomi': '', 'svenska': 'language=sv', 'english': 'language=en', 'russian': 'language=ru',
-             'samegiella': 'language=se', 'karjala': 'language=krl', 'kaikki': 'language=all'}
-    print('Choose language of the articles:\n' + '\n'.join(lang) + "\n")
-    url.append(get_input(lang))
-    
-    print('Input search query:')
-    queryurl="query="
-    query=input()
-    url.append(queryurl + query.replace(" ", "%20"))
-    url = [valid for valid in url if valid != '']
-    url = "&".join(url)
-    test = f'https://haku.yle.fi/?{url}&type=article&page=1'
-    print(test)
 
 
