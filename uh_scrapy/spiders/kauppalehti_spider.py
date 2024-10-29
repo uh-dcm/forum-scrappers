4575e2bbf674264a138ccc77a8e84a576aec9614
DennisDavydov <denisdavydov110893@gmail.com>
2024-10-29 20:37:23 +0200

    kauppalehti spider finished

--- uh_scrapy/spiders/kauppalehti_spider.py
+++ uh_scrapy/spiders/kauppalehti_spider.py
@@ -3,8 +3,8 @@ import scrapy
 from pathlib import Path
 import pandas as pd
 from scrapy.http import FormRequest
+import logging
 
-items = []
 class KauppalehtiSpider(scrapy.Spider):
 
     name = "kauppalehti"
@@ -12,6 +12,7 @@ class KauppalehtiSpider(scrapy.Spider):
 
     def __init__(self, search, *args, **kwargs):
         super(KauppalehtiSpider, self).__init__(*args, **kwargs)
+        logging.getLogger('scrapy').setLevel(logging.WARNING)
         self.search = search
         self.items = []
 
@@ -19,15 +20,23 @@ class KauppalehtiSpider(scrapy.Spider):
     def parse(self, response):
         print(response)
         _xfToken = response.css("input[name='_xfToken']::attr(value)").get()
+        if not _xfToken:
+            self.logger.error("Could not retrieve _xfToken")
+            return
         formdata = {
             'keywords': self.search[0],
             'c[title_only]': self.search[1],
             'c[users]': self.search[2],
             'c[newer_than]': self.search[3],
             'c[older_than]': self.search[4],
-            'order': self.search[5],
+            'c[min_reply_count]': self.search[5],
+            'c[nodes][]':self.search[6],
+            'c[child_nodes]':self.search[7],
+            'order': self.search[8],
+            'grouped': '1',
             '_xfToken' : _xfToken,
         }
+        print(formdata)
         yield FormRequest(
             url='https://keskustelu.kauppalehti.fi/search/search',
             formdata=formdata,
@@ -37,11 +46,13 @@ class KauppalehtiSpider(scrapy.Spider):
 
 
     def parse_threads(self, response):
+        print("Parsing threads now")
         threads = response.xpath("//li[contains(@class, 'block-row--separated')]")
         for thread in threads:
             link = thread.xpath(".//h3[@class='contentRow-title']/a/@href").get()
             url = response.urljoin(link)
-            thread_name = thread.xpath(".//h3[@class='contentRow-title']/a/text()").get()
+            thread_name = thread.xpath(".//h3[@class='contentRow-title']/a/text()").getall()
+            thread_name = ' '.join([text.strip() for text in thread_name if text.strip()]),
             yield scrapy.Request(url, callback=self.scrape_thread, meta={'thread': thread_name})
 
         self.parse_threads_next_page(response)
@@ -56,20 +67,20 @@ class KauppalehtiSpider(scrapy.Spider):
 
 
     def scrape_thread(self, response):
-
-        for comment in response.xpath('//div[@class="message-cell message-cell--main"]'):
-            body = comment.xpath('.//div[@class="message-body js-selectToQuote"]//div[@itemprop="text"]//div[@class="bbWrapper"]//text()').getall()
+        thread = response.xpath("//div[@class='p-title ']/h1/text()").get()
+        for comment in response.xpath("//div[@class='message-inner']"):
+            body = comment.xpath(".//article[@class='message-body js-selectToQuote']//div[@class='bbWrapper']//text()").getall()
             row = {
-                "thread": response.meta['thread'],
-                "Author": comment.xpath('.//div[@class="message-userContent"]/@data-lb-caption-desc').re_first(r'^(.*) ·'),
+                "thread": thread,
+                "Author": comment.xpath(".//h4[@class='message-name']/a//text()").get(),
                 "body": ' '.join([text.strip() for text in body if text.strip()]),
-                "id": comment.xpath('.//div[@class="message-userContent"]/@data-lb-id').get(),
-                "timestamp": comment.xpath('.//time[@class="u-dt"]/@datetime').get()
+                "id": comment.xpath(".//a[contains(@class, 'message-attribution-gadget')]/@data-href").re_first(r'/posts/(\d+)/'),
+                "timestamp": comment.xpath(".//time[@class='u-dt']/@datetime").get()
             }
             self.items.append(row)
+        yield from self.scrape_thread_next_page(response)
 
-            self.scrape_thread_next_page(response)
-
+    #check if the next page exists
     def scrape_thread_next_page(self, response):
         next_page = response.xpath("//a[contains(@class, 'pageNav-jump--next')]/@href").get()
         if next_page is not None:
@@ -95,7 +106,8 @@ class KauppalehtiSpider(scrapy.Spider):
     # Make filename and save data after spider is done
     def closed(self, reason):
         name = self.make_filename()
-        self.to_4cat_csv(self.items, name)
+        if self.items is not []:
+            self.to_4cat_csv(self.items, name)
 
 
 
