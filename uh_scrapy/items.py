bf17cafb540b5919fbbb5cd10687398be60eecc1
Denis Davydov <denisdavydov110893@gmail.com>
2024-11-08 02:54:32 +0200

    added item support

--- uh_scrapy/items.py
+++ uh_scrapy/items.py
@@ -6,6 +6,13 @@
 import scrapy
 
 
+class PostItem(scrapy.Item):
+    thread = scrapy.Field()
+    author = scrapy.Field()
+    body = scrapy.Field()
+    id = scrapy.Field()
+    timestamp = scrapy.Field()
+
 class Uh_scrapyItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
